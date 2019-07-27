8ff17a95d3bc29e1b977874d7b294681b0205c06
Laura <laura.robertson64@gmail.com>
2019-07-27 16:27:50 -0500

    custom error handling hook

---
+++ src/utils/custom-hooks/ErrorHandler.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+const userErrorHandler = (initialState: string | null) => {
+  const [error, setError] = React.useState(initialState);
+  const showError = (errorMessage: string | null) => {
+    setError(errorMessage);
+    window.setTimeout(() => {
+      setError(null);
+    }, 5000);
+  };
+  return { error, showError };
+};
+
+const { error, showError } = useErrorHandler(null);
