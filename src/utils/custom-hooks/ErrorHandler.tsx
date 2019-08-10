0edd0691ce0580d2764c3fcd89eee4bd649e1b0f
Laura <laura.robertson64@gmail.com>
2019-08-10 16:13:41 -0500

    updated hook name and removed extra errant line

--- src/utils/custom-hooks/ErrorHandler.tsx
+++ src/utils/custom-hooks/ErrorHandler.tsx
@@ -1,16 +1,13 @@
 import * as React from "react";
-
-const userErrorHandler = (initialState: string | null) => {
+const useErrorHandler = (initialState: string | null) => {
   const [error, setError] = React.useState(initialState);
   const showError = (errorMessage: string | null) => {
     setError(errorMessage);
     window.setTimeout(() => {
       setError(null);
-    }, 5000);
+    }, 3000);
   };
   return { error, showError };
 };
 
-const { error, showError } = useErrorHandler(null);
-
-export default userErrorHandler;
+export default useErrorHandler;
